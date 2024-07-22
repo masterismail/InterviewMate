3176c4737e97ea5f41536f531486692defe7be8b
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-07-22 13:11:19 +0530

    modified files

--- STT.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client = OpenAI()
-
-audio_file = open("/home/tahseer/Desktop/InterviewMate/1.1.2.mp3", "rb")
-transcription = client.audio.transcriptions.create(
-  model="whisper-1", 
-  file=audio_file
-)
-print(transcription.text)
--- interview-mate/components/SwitchMode.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useState } from "react";
-import { Button } from "./ui/button";
-
-export default function SwitchMode() {
-	return <div></div>;
-}
